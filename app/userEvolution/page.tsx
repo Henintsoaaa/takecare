b18ec9e5f842cc2b190fbf16e1a700bfaaf10193
Kimbohy <lovabarwijons@gmail.com>
2024-11-22 10:52:21 +0300

    Remove Sidebar component from UserEvolution page

--- app/userEvolution/page.tsx
+++ app/userEvolution/page.tsx
@@ -1,10 +1,8 @@
 import EmotionEvaluation from "@/components/EmotionEvaluation";
-import Sidebar from "@/components/Sidebar";
 const Page = () => {
   const currentPath = "/userEvolution";
   return (
     <div>
-      <Sidebar activePath={currentPath} />
       <EmotionEvaluation />
     </div>
   );
