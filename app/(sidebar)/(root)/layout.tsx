1f53d1d98642b2ee4e14c56d5168e65edc219229
Kimbohy <lovabarwijons@gmail.com>
2024-11-27 08:54:17 +0300

    fix: update layout for full height and enhance scrollbar styles in global CSS

--- app/(sidebar)/(root)/layout.tsx
+++ app/(sidebar)/(root)/layout.tsx
@@ -14,7 +14,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   }, []);
 
   return (
-    <div className="flex flex-col max-h-screen text-black">
+    <div className="flex flex-col h-full text-black">
       <Navbar />
       <div className="flex-grow overflow-y-scroll bg-gray-100">{children}</div>
     </div>
